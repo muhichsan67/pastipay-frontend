f40b70a18d99192d4af48eb18e2a34dd9cb01476
Muhammad Ichsan Fathurrochman <muhichsan67@gmail.com>
2026-07-05 18:01:12 +0700

    3rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './presentation/pages/Home';
 import { Pricing } from './presentation/pages/Pricing';
